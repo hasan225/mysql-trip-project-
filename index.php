4a26bee1d93c35d59a2e4035814f02e62ee16fae
bijoy <uzumakibijoy2017@gmail.com>
2021-05-10 01:44:19 +0600

    mysql trip project complete

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
 <?php
-        $inserts = false;
-if (isset($_POST['name'])) {
-   
-
+$inserts = false;
+$email = false;
+if ( isset( $_POST['name'] ) ) {
 
     $servername = "localhost";
     $username = "root";
     $password = "";
     $database = "trip";
 
-    $conn = mysqli_connect($servername, $username, $password, $database);
+    $conn = mysqli_connect( $servername, $username, $password, $database );
 
-    if (!$conn) {
-        die("sorry" . mysqli_connect_error());
+    if ( !$conn ) {
+        die( "sorry" . mysqli_connect_error() );
     }
     // else { echo "okay to go";}
 
-    $name = $_POST['name'];
-    $age = $_POST['age'];
-    $gender = $_POST['gender'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
-    $phone = $_POST['phone'];
-    $desc = $_POST['desc'];
+    if ( $email ) {
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $phone = $_POST['phone'];
+        $desc = $_POST['desc'];
 
-    $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `password`, `phone`, `desc` ,`dt`) VALUES ('$name', '$age', '$gender', '$email', '$password', '$phone', '$desc',current_timestamp())";
+        $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `password`, `phone`, `desc` ,`dt`) VALUES ('$name', '$age', '$gender', '$email', '$password', '$phone', '$desc',current_timestamp())";
 
-    // echo $sql;
+        // echo $sql;
 
-    if ($conn->query($sql) == true) {
-        // echo "success";
-        $inserts = true;
+        if ( $conn->query( $sql ) == true ) {
+            // echo "success";
+            $inserts = true;
+        } else {
+            echo "error: $sql <br> $conn->error";
+        }
+        $conn->close();
     } else {
-        echo "error: $sql <br> $conn->error";
+        echo "<h2 class='errorh'>please fill up all the informations carefully</h2>";
     }
-    $conn->close();
 }
-
 
 ?>
 
@@ -50,8 +53,35 @@ if (isset($_POST['name'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+
+    <style>
+    p.text-center.text-dark.rounded.bg-info.p-2 {
+        background: aquamarine !important;
+        padding: 7px !important;
+        border-radius: 10px;
+        letter-spacing: 1px;
+        color: black;
+        font-family: ui-monospace;
+    }
+
+    h2.errorh {
+        color: red;
+        letter-spacing: 2px;
+        font-size: 19px;
+        background: white;
+        width: 473px;
+        padding: 10px;
+        border-radius: 10px;
+        font-family: emoji;
+    }
+
+    div#emailHelp {
+        color: white;
+    }
+    </style>
     <title>welcome to zland</title>
 </head>
 
@@ -62,7 +92,9 @@ if (isset($_POST['name'])) {
         <h1 class="text-center">Welcome To Zland</h1>
         <h5 class="text-center">Be Ready To Get the Best Feeling Ever</h5>
 
-        <?php if ($inserts==true) {echo ' <p class="text-center">thanks for submitting the form let\'s hope for safe & nice trip stay turned see you soon  </p>';} ?>
+        <?php if ( $inserts == true ) {
+    echo ' <p class="text-center text-dark rounded bg-info p-2">thanks for submitting the form let\'s hope for safe & nice trip stay turned see you soon  </p>';
+} ?>
 
         <form action="index.php" method="POST">
             <div class="mb-3">
@@ -109,7 +141,9 @@ if (isset($_POST['name'])) {
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous">
+    </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
